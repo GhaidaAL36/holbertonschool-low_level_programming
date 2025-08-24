c4d914cce11adf09e008309a877881d8b91229b5
GhaidaAl36 <ghaida3638@gmail.com>
2025-08-24 15:28:30 +0000

    fix task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main
  *
  * Return: Always 0
  */
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n - rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
