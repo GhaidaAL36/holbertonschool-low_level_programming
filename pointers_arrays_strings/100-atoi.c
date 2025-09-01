35aa060c0a55c27a53dbdcf7cb48bec43674558b
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-01 13:19:56 +0000

    task 10 fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,22 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1, num = 0;
 
-	while (s[i] != '\0')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			while (s[i] >= '0' && s[i] <= '9')
-			{
-				result = result * 10 + (s[i] - '0');
-				i++;
-			}
-			break;
-		}
+		else if (s[i] == '+')
+			sign *= 1;
 		i++;
 	}
 
-	return (result * sign);
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * num);
 }
