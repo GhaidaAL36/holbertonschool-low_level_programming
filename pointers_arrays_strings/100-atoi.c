7e9ae253408c7ab5bb86366767d7da841d7bab9e
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-01 13:11:54 +0000

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: input string
+ *
+ * Return: the integer value
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = result * 10 + (s[i] - '0');
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+
+	return (result * sign);
+}
