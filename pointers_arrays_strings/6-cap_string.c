c6457ded9931d8f71692260f66280e586f4c427f
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-01 13:53:11 +0000

    task 7 part 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to change
+ *
+ * Return: pointer to the changed string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+	while (s[i])
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+		     s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+		     s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+		     s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+		     s[i - 1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+	return (s);
+}
