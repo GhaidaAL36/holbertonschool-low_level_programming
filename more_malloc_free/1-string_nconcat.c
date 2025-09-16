6ef3f893e3022b8995566a09f8ef6e2a44c3ba43
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-16 07:42:51 +0000

    last fix for task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -11,42 +10,35 @@
  * Return: pointer to newly allocated memory containing s1 followed
  *         by the first n bytes of s2, NULL on failure
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
+	unsigned int copy_len;
 	unsigned int i, j;
-	unsigned int copy_len = 0;
 	char *concat;
 
-	len1 = (s1 != NULL) ? strlen(s1) : 0;
-	len2 = (s2 != NULL) ? strlen(s2) : 0;
-
-
-	if (n > len2)
-		copy_len = len2;
-	else
-		copy_len = n;
-
-	concat = malloc((len1 + copy_len + 1) * sizeof(char));
+	if (s1)
+	{
+		while (s1[len1] != '\0')
+			len1++;
+	}
+	if (s2)
+	{
+		while (s2[len2] != '\0')
+			len2++;
+	}
+	copy_len = (n < len2) ? n : len2;
+	concat = malloc(len1 + copy_len + 1);
 
 	if (!concat)
 		return (NULL);
-
-	if (s1)
 	for (i = 0; i < len1; i++)
-	{
 		concat[i] = s1[i];
-	}
-	if (s2)
 	for (j = 0; j < copy_len; j++)
-	{
 		concat[len1 + j] = s2[j];
-	}
 
 	concat[len1 + copy_len] = '\0';
 
 	return (concat);
-
 }
