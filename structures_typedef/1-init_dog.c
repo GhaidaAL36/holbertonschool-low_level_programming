89858de6557eaccd7308b6d1b90b1bf0e8e69c63
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-17 14:10:05 +0000

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * struct dog - Represents a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the dog struct
+ * @name: Name to assign
+ * @age: Age to assign
+ * @owner: Owner to assign
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+};
