bdeecc75ca723cd2e71b1658df9dd79e384ddece
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-21 07:50:00 +0000

    fix betty style

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,31 +2,31 @@
 
 int main(int argc, char *argv[])
 {
-    int (*f)(int, int);
-    int num1, num2;
+	int (*f)(int, int);
+	int num1, num2;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    f = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
-    if (!f)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    printf("%d\n", f(num1, num2));
-    return (0);
+	printf("%d\n", f(num1, num2));
+	return (0);
 }
