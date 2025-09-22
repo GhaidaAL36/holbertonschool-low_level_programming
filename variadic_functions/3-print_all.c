6c2b8c9fe7bb55339af1d07685a0a5e621ae69f5
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-22 08:43:41 +0000

    last fix for task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,44 +3,34 @@
 #include <stdio.h>
 
 /**
- * print_all - prints any type of argument based on format string
- * @format: string of types (c: char, i: int, f: float, s: string)
- *
- * Description: prints each argument separated by ", ".
- * If a string is NULL, prints (nil). Prints a newline at the end.
- * No else/else if is used. Maximum 2 if statements and < 9 variables.
+ * print_all - prints any type based on format
+ * @format: string of type identifiers (c, i, f, s)
  */
 void print_all(const char * const format, ...)
 {
     va_list args;
-    unsigned int i = 0;
+    unsigned int j = 0;
     char *str;
-    char *sep = "";
-
-    if (!format)
-        return;
+    int printed = 0;
 
     va_start(args, format);
 
-    while (format[i])
+    while (format && format[j])
     {
-        if (format[i] == 'c')
-            printf("%s%c", sep, va_arg(args, int));
-
-        if (format[i] == 'i')
-            printf("%s%d", sep, va_arg(args, int));
-
-        if (format[i] == 'f')
-            printf("%s%f", sep, va_arg(args, double));
-
-        if (format[i] == 's')
+        if (format[j] == 'c')
+            printed += printf("%s%c", printed ? ", " : "", va_arg(args, int));
+        if (format[j] == 'i')
+            printed += printf("%s%d", printed ? ", " : "", va_arg(args, int));
+        if (format[j] == 'f')
+            printed += printf("%s%f", printed ? ", " : "", va_arg(args, double));
+        if (format[j] == 's')
         {
             str = va_arg(args, char *);
-            printf("%s%s", sep, str ? str : "(nil)");
+            if (!str)
+                str = "(nil)";
+            printed += printf("%s%s", printed ? ", " : "", str);
         }
-
-        sep = ", ";
-        i++;
+        j++;
     }
 
     va_end(args);
