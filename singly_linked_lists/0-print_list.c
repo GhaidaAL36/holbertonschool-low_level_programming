d4ddffbd5639336dcf37b586738ac28568a6aa80
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-06 13:26:38 +0000

    fix betty doc

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the first node of the list
+ *
+ * Return: the number of nodes
+ * Format: [len] string (if str is NULL, print [0] (nil))
+ */
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
