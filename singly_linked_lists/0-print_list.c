224f8feb5257014395d4bac2e89ddd4a98525ee0
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-06 13:24:10 +0000

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != 0)
+	{
+		if (h->str != 0)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
