7533eda19fac61ac9c0957970c3c3d96b394bf45
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-08 07:06:26 +0000

    ??

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,38 +7,25 @@
 #define BUF_SIZE 1024
 
 /**
- * error_exit - prints an error message and exits
- * @msg: the error message
- * @file: filename related to the error
+ * print_error_and_exit - prints error message and exits with code
+ * @msg: message string
+ * @file: file name
  * @code: exit code
  */
-void error_exit(const char *msg, const char *file, int code)
+void print_error_and_exit(const char *msg, const char *file, int code)
 {
 	dprintf(STDERR_FILENO, "Error: %s %s\n", msg, file);
 	exit(code);
 }
 
 /**
- * safe_close - closes a file descriptor and exits if it fails
- * @fd: the file descriptor
- */
-void safe_close(int fd)
-{
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-/**
- * copy_file - copies content from fd_from to fd_to
+ * copy_content - copies content from fd_from to fd_to
  * @fd_from: source file descriptor
  * @fd_to: destination file descriptor
- * @file_from: source filename for error messages
- * @file_to: destination filename for error messages
+ * @file_from: source file name (for error messages)
+ * @file_to: destination file name (for error messages)
  */
-void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
 {
 	ssize_t n_read, n_written;
 	char buffer[BUF_SIZE];
@@ -47,18 +34,29 @@ void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
 	{
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written != n_read)
-			error_exit("Can't write to", file_to, 99);
+			print_error_and_exit("Can't write to", file_to, 99);
 	}
 	if (n_read == -1)
-		error_exit("Can't read from file", file_from, 98);
+		print_error_and_exit("Can't read from file", file_from, 98);
 }
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: arguments (file_from file_to)
+ * close_fd - closes a file descriptor and handles errors
+ * @fd: file descriptor to close
+ * @file: file name (for error messages)
+ */
+void close_fd(int fd, char *file)
+{
+	if (close(fd) == -1)
+		print_error_and_exit("Can't close fd", file, 100);
+}
+
+/**
+ * main - copies content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 0 on success, exits with proper code on failure
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
@@ -72,19 +70,18 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit("Can't read from file", argv[1], 98);
+		print_error_and_exit("Can't read from file", argv[1], 98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		safe_close(fd_from);
-		error_exit("Can't write to", argv[2], 99);
+		close_fd(fd_from, argv[1]);
+		print_error_and_exit("Can't write to", argv[2], 99);
 	}
 
-	copy_file(fd_from, fd_to, argv[1], argv[2]);
-
-	safe_close(fd_from);
-	safe_close(fd_to);
+	copy_content(fd_from, fd_to, argv[1], argv[2]);
+	close_fd(fd_from, argv[1]);
+	close_fd(fd_to, argv[2]);
 
 	return (0);
 }
