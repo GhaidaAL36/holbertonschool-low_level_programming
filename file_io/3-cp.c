5dda513f5dfc53f5a894eda7e0069b0fcd233136
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-14 06:26:31 +0000

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,18 +1,19 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 1024
 
 /**
- * close_file - Closes a file descriptor and handles errors.
+ * close_file - Closes a file descriptor or exits with code 100.
  * @fd: File descriptor to close.
  */
 void close_file(int fd)
 {
-	int c = close(fd);
-
-	if (c == -1)
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -20,85 +21,98 @@ void close_file(int fd)
 }
 
 /**
- * error_read - Handles read errors.
- * @file_from: Source file name.
+ * exit_error - Prints an error message and exits with the given code.
+ * @code: Exit code (97, 98, 99, or 100)
+ * @msg: Error message (Usage, Can't read/write/close)
+ * @file: Related filename (can be NULL)
  */
-void error_read(char *file_from)
+void exit_error(int code, const char *msg, const char *file)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-	exit(98);
+	if (file)
+		dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+	else
+		dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(code);
 }
 
 /**
- * error_write - Handles write/create errors.
- * @file_to: Destination file name.
+ * open_dest - Opens destination file with correct perms.
+ * Ensures 0664 perms only if newly created.
+ * @path: Destination filename
+ * Return: File descriptor.
  */
-void error_write(char *file_to)
+int open_dest(const char *path)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-	exit(99);
+	int fd;
+
+	fd = open(path, O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (fd != -1)
+	{
+		if (fchmod(fd, 0664) == -1)
+		{
+			close(fd);
+			exit_error(99, "Error: Can't write to", path);
+		}
+		return (fd);
+	}
+
+	if (errno == EEXIST)
+	{
+		fd = open(path, O_WRONLY | O_TRUNC);
+		if (fd == -1)
+			exit_error(99, "Error: Can't write to", path);
+		return (fd);
+	}
+	exit_error(99, "Error: Can't write to", path);
+	return (-1);
 }
 
 /**
- * open_files - Opens source and destination files.
- * @file_from: source filename
- * @file_to: destination filename
- * @fd_from: pointer to source fd
- * @fd_to: pointer to dest fd
+ * copy_contents - Copies data from src_fd to dst_fd.
+ * @src_fd: Source file descriptor.
+ * @dst_fd: Destination file descriptor.
+ * @src_name: Source filename.
+ * @dst_name: Destination filename.
  */
-void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
+void copy_contents(int src_fd, int dst_fd, const char *src_name,
+		   const char *dst_name)
 {
-	*fd_from = open(file_from, O_RDONLY);
-	if (*fd_from == -1)
-		error_read(file_from);
+	ssize_t r, w;
+	char buf[BUF_SIZE];
 
-	*fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (*fd_to == -1)
-		error_write(file_to);
+	while ((r = read(src_fd, buf, BUF_SIZE)) > 0)
+	{
+		w = write(dst_fd, buf, r);
+		if (w == -1 || w != r)
+			exit_error(99, "Error: Can't write to", dst_name);
+	}
+	if (r == -1)
+		exit_error(98, "Error: Can't read from file", src_name);
 }
 
 /**
- * main - Copies the content of a file to another file.
+ * main - Copies the content of one file into another.
  * @argc: Argument count.
- * @argv: Argument vector.
- * Return: 0 on success.
+ * @argv: Argument values.
+ * Return: 0 on success, exits on error.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int fd_from, fd_to, rd, wr;
-	char *buffer;
+	int fd_from, fd_to;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit_error(97, "Usage: cp file_from file_to", NULL);
 
-	buffer = malloc(1024);
-	if (buffer == NULL)
-		error_write(argv[2]);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		exit_error(98, "Error: Can't read from file", argv[1]);
 
-	open_files(argv[1], argv[2], &fd_from, &fd_to);
+	fd_to = open_dest(argv[2]);
+	copy_contents(fd_from, fd_to, argv[1], argv[2]);
 
-	rd = read(fd_from, buffer, 1024);
-	while (rd > 0)
-	{
-		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
-		{
-			free(buffer);
-			error_write(argv[2]);
-		}
-		rd = read(fd_from, buffer, 1024);
-	}
-	if (rd == -1)
-	{
-		free(buffer);
-		error_read(argv[1]);
-	}
-
-	free(buffer);
 	close_file(fd_from);
 	close_file(fd_to);
+
 	return (0);
 }
+
