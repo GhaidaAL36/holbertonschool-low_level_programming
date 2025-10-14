ea8c0e96f6f26a85a228e86d7a1c8d2bd906e30f
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-14 07:14:34 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,7 +20,81 @@ static void close_or_die(int fd)
 }
 
 /**
- * main - copy content of a file to another file
+ * open_source - open source file for reading
+ * @file: filename
+ * Return: file descriptor
+ */
+static int open_source(const char *file)
+{
+	int fd = open(file, O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_dest - open/create destination file for writing
+ * @file: filename
+ * Return: file descriptor
+ */
+static int open_dest(const char *file)
+{
+	int fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * copy_content - copy from src_fd to dest_fd
+ * @src_fd: source file descriptor
+ * @dest_fd: destination file descriptor
+ * @src_file: source filename (for error messages)
+ * @dest_file: destination filename (for error messages)
+ */
+static void copy_content(int src_fd, int dest_fd, const char *src_file,
+			const char *dest_file)
+{
+	ssize_t r, w;
+	char buf[BUF];
+
+	while ((r = read(src_fd, buf, BUF)) > 0)
+	{
+		ssize_t off = 0;
+
+		while (off < r)
+		{
+			w = write(dest_fd, buf + off, r - off);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
+				close_or_die(src_fd);
+				close_or_die(dest_fd);
+				exit(99);
+			}
+			off += w;
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
+		close_or_die(src_fd);
+		close_or_die(dest_fd);
+		exit(98);
+	}
+}
+
+/**
+ * main - entry point
  * @ac: argument count
  * @av: argument vector
  * Return: 0 on success
@@ -28,8 +102,6 @@ static void close_or_die(int fd)
 int main(int ac, char **av)
 {
 	int f_from, f_to;
-	ssize_t r, w;
-	char buf[BUF];
 
 	if (ac != 3)
 	{
@@ -37,51 +109,9 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	/* open source file */
-	f_from = open(av[1], O_RDONLY);
-	if (f_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
-	/* open/create destination file with truncate */
-	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (f_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close_or_die(f_from);
-		exit(99);
-	}
-
-	/* read from source and write to destination in 1024 bytes */
-	while ((r = read(f_from, buf, BUF)) > 0)
-	{
-		ssize_t offset = 0;
-
-		/* handle short writes */
-		while (offset < r)
-		{
-			w = write(f_to, buf + offset, r - offset);
-			if (w == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-				close_or_die(f_from);
-				close_or_die(f_to);
-				exit(99);
-			}
-			offset += w;
-		}
-	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		close_or_die(f_from);
-		close_or_die(f_to);
-		exit(98);
-	}
-
+	f_from = open_source(av[1]);
+	f_to = open_dest(av[2]);
+	copy_content(f_from, f_to, av[1], av[2]);
 	close_or_die(f_from);
 	close_or_die(f_to);
 
