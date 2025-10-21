1368af488a84819779d69756b58f738c8e3b9514
GhaidaAl36 <ghaida3638@gmail.com>
2025-10-21 07:27:56 +0000

    adding betty doc

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,14 @@
+/**
+ * hash_djb2 - Generates a hash value using the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Description: Implements the djb2 algorithm by
+ * initializing the hash to 5381 and then iterating
+ * through each character of the string, updating
+ * the hash value with hash * 33 + current_char.
+ *
+ * Return: The computed hash value as an unsigned long int.
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
